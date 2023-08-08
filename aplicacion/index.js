77ad9388860fb94b96e88127f034617e49f740f0
victoriaavalee <victoriaavalee754@gmail.com>
2023-08-08 02:57:01 -0300

    login y restablecer contraseña agregados

--- aplicacion/index.js
+++ aplicacion/index.js
@@ -1,17 +1,18 @@
 const express = require ('express');
 const path= require ('path');
 const app = express ();
-app.use(express.static('public'))
+const publicFolderPath=path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
 
-app.get ('/', (req, res)=>{ //home
-    res.sendFile(path.join (__dirname, './views/home.html'))
+app.get ('/', (req, res)=>{ //home /inicio
+    res.sendFile(path.resolve (__dirname, './views/home.html'))
 });
 
-app.get ('/login', (req, res)=>{
-    res.sendFile(path.join (__dirname, './views/login.html'))
+app.get ('/login', (req, res)=>{ //login/acceder
+    res.sendFile(path.resolve (__dirname, './views/login.html'))
 });
 
-app.get ('/registro', (req, res)=>{
+app.get ('/registro', (req, res)=>{//registrarse
     res.sendFile(path.join (__dirname, './views/registro.html'))
 });
 
@@ -19,10 +20,27 @@ app.get ('/producto', (req, res)=>{
     res.sendFile(path.join (__dirname, './views/producto.html'))
 });
 
-app.get ('/carrito', (req, res)=>{
+app.get ('/carrito', (req, res)=>{//carrito
     res.sendFile(path.join (__dirname, './views/carrito.html'))
 });
 
+
+
+app.get ('/restablecer-clave', (req, res)=>{//indumentaria
+    res.sendFile(path.join (__dirname, './views/restablecer-clave.html'))
+});
+app.get ('/sobreNosotros', (req, res)=>{//sobre nosotros
+    res.sendFile(path.join (__dirname, './views/sobreNosotros.html'))
+});
+app.get ('/variedades', (req, res)=>{//variedades
+    res.sendFile(path.join (__dirname, './views/variedades.html'))
+});
+app.get ('/accesorios', (req, res)=>{//accesorios
+    res.sendFile(path.join (__dirname, './views/accesorios.html'))
+});
+app.get ('/indumentaria', (req, res)=>{//indumentaria
+    res.sendFile(path.join (__dirname, './views/indumentaria.html'))
+});
 
 
 app.listen (8080, () =>{
