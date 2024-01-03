9326671477cda12d488bfe7a3b446f7032dbdea2
paola-zoe <paolazoetoconas@gmail.com>
2024-01-03 04:35:54 -0300

    Multer

--- aplicacion/src/middleware/productsMulter.js
+++ aplicacion/src/middleware/productsMulter.js
@@ -1,19 +1,14 @@
 const multer = require ('multer');
-const path = require ('path');
-
-
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, '../public/img/')
+        cb(null, '../public/img/products/'); 
     },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
 
-    filename: function (req, file, cb) {
-        cb(null,
-            `${Date.now()}${path.extname(file.originalname)}`);
-        }
-})
-
-const uploadFile = multer({ storage });
+const uploadFile = multer({ storage: storage });
 
 module.exports = uploadFile;
